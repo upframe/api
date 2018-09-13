bd54c694a8e61af28fbe6820f58e81e2dff10774
Ulisses Ferreira <ulissesvf@gmail.com>
2018-09-13 01:33:36 +0100

    test: proposed file structure

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ require('dotenv').config(); //Podemos usar .env para as variables
 
 const port = process.env.PORT || 80;
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
 const app = express()
 
-/* ROUTER ZONE 
-   Pelo que tenho visto de REST e pelo que eu tenho sempre feito
-   a melhor prática é dividires a API em /users/ para tudo users
-   related, /admin/ para tudo admin related, etc e particionar
-   essas rotas diferentes em routers de javascript diferentes.
-   Isto permite que as coisas fiquem mais bem organizadas e que
-   dês handle dos routing separados*/
+/* Configs */
+app.use(cors({ credentials: true, origin: `https://connect.upframe.io` }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
+/* Router */
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter)
-
-/* ROUTER ZONE */
 
 app.listen(port, () => { console.log('API listening on port ' + port)})
---
+++ models/users.js
@@ -0,0 +1,17 @@
+class Users {
+
+  constructor(name, email, password) {
+
+  }
+
+  set name (name) {
+
+  }
+
+  get name () {
+    return 'Martin Batata'
+  }
+
+}
+
+module.exports = Users;
