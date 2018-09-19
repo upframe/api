833517f1f5a8653be7c522cf8e74445a04233017
Ulisses Ferreira <ulissesvf@gmail.com>
2018-09-19 14:54:56 +0100

    feat: add missing route

--- routes/mentor.js
+++ routes/mentor.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.status(200).send('Tudo correu bem')
+})
+
 router.get('/random', (req, res) => {
   res.status(200).send('Tudo correu bem')
 })
