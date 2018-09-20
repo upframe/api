40fbe3ea6739bf417e7bf82ce0f48d3789b2d984
Ulisses Ferreira <ulissesvf@gmail.com>
2018-09-20 19:50:06 +0100

    feat: GET /mentor/random

--- routes/mentor.js
+++ routes/mentor.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const router = express.Router()
-//const db = require('../services/db.js')             - enable when you start working
-//const verifyToken = require('../services/token.js') - enable when you start working
+const db = require('../services/db.js')
 
 router.get('/', (req, res) => {
-  res.status(200).send('Tudo correu bem')
+  let sql = 'SELECT * FROM users'
+  db.query(sql, (err, result) => {  
+    console.log(result)
+    res.status(200).send('Tudo bem')
+  })
 })
 
 router.get('/random', (req, res) => {
-  res.status(200).send('Tudo correu bem')
+  let sql = 'SELECT name, role, company, bio, tags, keycode, profilePic FROM users'
+  db.query(sql, (err, result) => {
+    console.log(result)
+    res.status(200).send(shuffle(result))
+  })
 })
 
 router.get('/info', (req, res) => {
@@ -22,5 +29,13 @@ router.get('/verify', (req, res) => {
 router.post('/meetup', (req, res) => {
   res.status(200).send('Tudo correu bem')
 })
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
 
 module.exports = router;
