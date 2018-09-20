7187f8024e7a79e40d84ddde975e6ee6b72a497e
Ulisses Ferreira <ulissesvf@gmail.com>
2018-09-20 19:13:58 +0100

    chore: add middleware on search route

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../services/db.js')
+const verifyToken = require('../services/token.js')
 
 router.get('/quick', (req, res) => {
   res.status(200).send('Tudo correu bem')
