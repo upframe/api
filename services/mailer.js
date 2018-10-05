d62572ce7daacc31f6575a77bc91ec3731b54b01
Fábio Ferreira <ffcfpten@gmail.com>
2018-10-06 00:47:08 +0100

    feat: reset password

--- services/mailer.js
+++ services/mailer.js
@@ -3,37 +3,58 @@ require('dotenv').config()
 
 const mailgun = require('mailgun-js')
 const fs = require('fs')
+const crypto = require('crypto')
 
 class Mailer {
   constructor(app) {
+    this.database = app.get('db').getPool()
     this.logger = app.get('logger');
     this.mailgun = mailgun({apiKey: process.env.MG_APIKEY, domain: process.env.MG_DOMAIN})
 
     if(this.logger) this.logger.verbose('Mail service loaded')
   }
 
-  getTemplate(name) {
-    return fs.readFileSync(`assets/${name}.html`, 'utf8')
+  getTemplate(name, args) {
+    let file = fs.readFileSync(`assets/${name}.html`, 'utf8')
+    if(args) {
+      for(let prop in args) {
+        file = file.replace(new RegExp(prop, 'g'), args[prop])
+      }
+    }
+    
+    return file
   }
 
   /**
    * @param {string} toAddress 
    */
   async sendPasswordReset(toAddress) {
-    let data = {
-      from: 'noreply@upframe.io',
-      to: toAddress,
-      subject: 'Password reset',
-      html: this.getTemplate('resetPassword')
-    }
+    let [rows] = await this.database.query('SELECT COUNT(*) FROM users WHERE email = ?', toAddress)
 
-    return this.mailgun.messages().send(data)
-      .then(data => {
-        if(data.message !== '' && data.id !== '') return 0
-        else throw 1
-      }).catch(() => {        
-        return 1
-      })
+    if(rows[0]['COUNT(*)']) {
+      try {
+        let data = {
+          from: 'noreply@upframe.io',
+          to: toAddress,
+          subject: 'Password reset'
+        },
+        token = crypto.randomBytes(20).toString('hex')
+  
+        data.html = this.getTemplate('resetPassword', {'RESETURL': token})
+        let [rows] = await this.database.query('INSERT INTO resetPassword VALUES(?,?)', [toAddress, token])
+        if(rows.affectedRows) {
+          return this.mailgun.messages().send(data)
+          .then(data => {
+            if(data.message !== '' && data.id !== '') return 0
+            else throw 1
+          }).catch(() => {        
+            return 1
+          }) 
+        }
+      } catch (err) {
+
+      }
+    } else return 1
   }
 }
 
