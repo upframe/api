7702e4ddeb70870065f76a7aee8604f4d47d18fa
Fábio Ferreira <ffcfpten@gmail.com>
2018-10-06 17:58:07 +0100

    feat: add sql placehodler creator util

---
+++ utils/sql.js
@@ -0,0 +1,22 @@
+
+createSQLPlaceholderFromJSON = (json) => {
+  let partial = '('
+  let params = []
+
+  // add property names from JSON
+  for (let prop in json) {
+    partial += prop + ','
+  }
+  partial = partial.slice(0, -1) + ') VALUES('
+
+  // add ? as placeholders to VALUES list
+  for(let prop in json) {
+    partial += '?,'
+    params.push(json[prop])
+  }
+  partial = partial.slice(0, -1) + ')'
+
+  return [partial, params]
+}
+
+module.exports.createSQLPlaceholderFromJSON = createSQLPlaceholderFromJSON
