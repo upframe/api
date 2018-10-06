bff15187e094249524fdb4a270105ae634d064cb
Fábio Ferreira <ffcfpten@gmail.com>
2018-10-06 17:56:49 +0100

    feat: promisifying login and register

--- services/user.js
+++ services/user.js
@@ -10,61 +10,23 @@ class User {
     if(this.logger) this.logger.verbose('User service loaded')
   }
 
-  create(req, res) {
-    let sql = 'INSERT INTO users (email, password, keycode, name) VALUES(?, ?, ?, ?)',
-      response = {
-        code: 200,
-        ok: 1
-      },
-      params = []
-    
-    for(let param in req.body) {
-      if(param !== 'password') params.push(req.body[param])
-      else {
-        // create password
-        let salt = bcrypt.genSaltSync(10)
-        let hash = bcrypt.hashSync(req.body[param], salt)
-
-        params.push(hash)
-      }
-    }
-    
-    this.database.getConnection((err, conn) => {
-      conn.query(sql, params, (err) => {
-        if(err) {
-          // duplicate entry
-          if(err.errno === 1062) {
-            response.code = 400;
-            response.message = 'Cannot insert duplicate entry'
-          }
-
-          res.status(response.code).json(response)
-          return
-        }
-
-        res.status(response.code).json(response)
-      })
-    })
-  }
-
-  get(req, res) {
+  async get(req, res) {
     let sql = 'SELECT * FROM users WHERE email = ?',
       response = {
         code: 200,
         ok: 1
       },
       token = jwt.decode(req.headers['authorization'].split('Bearer ')[1])
+    
+    let [rows] = await this.database.query(sql, token.email)
+    if(rows.length) {
+      response.user = rows[0]
+    } else {
+      response.ok = 0
+      response.code = 404
+    }
 
-    this.database.getConnection((err, conn) => {
-      conn.query(sql, token.email, (err, result) => {
-        if(err) res.status(500)
-
-        response.user = result[0]
-        res.status(response.code).send(response)
-      })
-
-      this.database.releaseConnection(conn)
-    })
+    res.status(response.code).json(response)
   }
 
   update(req, res) {
