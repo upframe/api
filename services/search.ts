07dde81db31419babb6cc19e24ca9e4a441d0511
Ulisses Ferreira <fc49430@alunos.fc.ul.pt>
2019-04-02 18:31:54 +0200

    fix: always return an array even with just one element in Full Seach

--- services/search.ts
+++ services/search.ts
@@ -80,7 +80,7 @@ export class SearchService extends Service {
 
     try {
       const sqlQuery = 'SELECT name, profilePic, bio, keycode, tags, role, company FROM users WHERE name LIKE ?'
-      const user = await this.database.query(sqlQuery, [`%${req.query.term}%`])
+      let user = await this.database.query(sqlQuery, [`%${req.query.term}%`])
       if (!Object.keys(user).length) {
         error = {
           api: true,
@@ -91,9 +91,10 @@ export class SearchService extends Service {
 
         throw error
       } else {
-        response.search = {
-          people: user,
+        if (!Array.isArray(user)) {
+          user = [user]
         }
+        response.search = user
       }
     } catch (err) {
       response = {
