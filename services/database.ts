eb12f75e5baf7c759bb87c156f84e597d514f868
Fábio Ferreira <ffcfpten@gmail.com>
2019-08-04 02:25:40 +0200

    refactor: database error detection

--- services/database.ts
+++ services/database.ts
@@ -1,24 +1,28 @@
 import * as express from 'express'
 import * as mysql from 'mysql2/promise'
+import { Logger } from 'winston'
 
 import { date } from '../types'
 
 export class Database {
   private pool: any
+  private logger!: Logger
 
   constructor(app: express.Application) {
     try {
+      this.logger = app.get('logger')
+
       const pool: mysql.Pool = mysql.createPool({
         host : process.env.DB_HOST,
         user : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
         database : process.env.DB_NAME,
       })
-      if (pool) app.get('logger').verbose('Database OK')
+      if (pool) this.logger.verbose('Database OK')
 
       this.pool = pool
     } catch (err) {
-      app.get('logger').error('Database NOT OK')
+      this.logger.error('Database NOT OK')
     }
   }
 
@@ -41,6 +45,20 @@ export class Database {
         else return {}
       }
     } catch (err) {
+      if (err.errno === 1251) {
+        this.logger.error('Database NOT OK')
+        this.logger.error(`MySQL error: ${err}`)
+
+        // Connection is bad because of drivers or authentication methods
+        return 2
+      } else if (err.errno === 1045) {
+        this.logger.error('Database NOT OK')
+        this.logger.error(`MySQL error: ${err}`)
+
+        // Access Denied to the DB
+        return 3
+      }
+
       return 1
     }
   }
