229b123d1fec1df9677d13cd328b5200c5da8fd2
MathisBullinger <mathis.bullinger@code.berlin>
2019-10-24 19:39:56 +0200

    refactor: switch tslint to eslint and add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+}
+
