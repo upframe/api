07fc30c09dc033f884025b042c27b0ee686ad89f
MathisBullinger <mathis.bullinger@code.berlin>
2019-11-01 22:08:44 +0100

    feat: add object utility functions & chainable interface

--- utils/index.ts
+++ utils/index.ts
@@ -2,5 +2,6 @@ import * as calendar from './calendar'
 import { logger } from './logs'
 import * as sql from './sql'
 import * as format from './format'
+import * as object from './object'
 
-export { calendar, logger, sql, format }
+export { calendar, logger, sql, format, object }
