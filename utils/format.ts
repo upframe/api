d3bdf2c6625d19a936636309d3af10e89dbd1a41
MathisBullinger <mathis.bullinger@code.berlin>
2019-11-01 22:39:26 +0100

    fix: update type annotations

--- utils/format.ts
+++ utils/format.ts
@@ -1,7 +1,8 @@
 import { Custom as Obj, filterKeys } from './object'
+import { Mentor } from '../types'
 
 // sort pictures into size -> type -> url structure
-export function pictures(data: object) {
+export function pictures(data: object): Mentor['pictures'] {
   let pics = new Obj(data)
     .filterKeys(k => k.startsWith('pic'))
     .mapKeys(k => k.replace('pic', '').toLowerCase())
@@ -14,13 +15,13 @@ export function pictures(data: object) {
   return Obj.fromKeys(jpeg.keys).mapValues((v, k) => ({
     jpeg: jpeg[k],
     webp: webp[k],
-  })).value
+  })).value as Mentor['pictures']
 }
 
 // extract pictures from mentor into pictures property
-export function mentor(data: object) {
+export function mentor(data: object): Mentor {
   return {
-    ...filterKeys(data, k => !k.startsWith('pic')),
+    ...(filterKeys(data, k => !k.startsWith('pic')) as Mentor),
     pictures: pictures(data),
   }
 }
